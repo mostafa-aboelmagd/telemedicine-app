2b2b5f3318995e48aabf3457f8acb1819e11d208
Youssef Hassanien <Youssefhassanein17@gmail.com>
2024-08-25 16:46:22 +0300

    Reverted last commit

--- functions.js
+++
@@ -1,9 +0,0 @@
-function passwordValidation(str) {
-    const hasNumbers = /\d/.test(str);
-    const hasAlphabets = /[a-zA-Z]/.test(str);
-    const hasSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(str);
-
-    return hasNumbers && hasAlphabets && hasSpecialChars && str.length >= 8;
-}
-
-module.exports = {passwordValidation };
