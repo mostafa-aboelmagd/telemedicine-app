232ce0b5bb3d10cbe08fbd92b2dc8c9f01288cd1
Youssef Hassanien <Youssefhassanein17@gmail.com>
2024-09-02 07:53:30 +0300

    Patient edit done

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const cookieParser = require('cookie-parser');
 const patientLoginRoute = require('./Routes/Patient/Login');
 const patientRegisterRoute = require('./Routes/Patient/Register');
 const patienProfileRoute = require('./Routes/Patient/Profile');
-// const patientEditRoute = require('./Routes/Patient/Edit');
+const patientEditRoute = require('./Routes/Patient/Edit');
 const doctorLoginRoute = require('./Routes/Doctor/Login');
 const doctorProfileRoute = require('./Routes/Doctor/Profile');
 const doctorEditRoute = require('./Routes/Doctor/Edit');
 const appointmentCreateRoute = require('./Routes/Appointment/Create');
 const port = 3000;
 const app = express();
+
+app.listen(port, (error) => {
+  if (error) {
+      console.error(error);
+      return;
+  }
+  console.log(`Server is running on port ${port}`);
+});
 
 app.use(express.json()); 
 app.use(cookieParser());
@@ -27,7 +35,7 @@ app.use(cookieParser());
 app.use('/patient/login', patientLoginRoute);
 app.use('/patient/register', patientRegisterRoute);
 app.use('/patient/profile', patienProfileRoute);
-// app.use('/patient/edit', patientEditRoute);
+app.use('/patient/edit', patientEditRoute);
 app.use('/doctor/login', doctorLoginRoute);
 app.use('/doctor/edit', doctorEditRoute);
 app.use('/doctor/profile', doctorProfileRoute);
@@ -40,13 +48,3 @@ app.use('/appointment/create', appointmentCreateRoute);
 //   const data = error.data;
 //   res.status(status).json({ message: message, data: data });
 // });
-
-
-
-app.listen(port, (error) => {
-  if (error) {
-      console.error(error);
-      return;
-  }
-  console.log(`Server is running on port ${port}`);
-});
