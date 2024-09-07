eb6230152f0b4638698da0a5abb651dc7c7fecc6
Mosafa Ayman <mostafaaboelmagd01@gmail.com>
2024-09-07 15:42:27 +0200

    Update Profile UI

--- telemedapp/components/doctorProfile/TimeSlots.tsx
+++ telemedapp/components/doctorProfile/TimeSlots.tsx
@@ -1,39 +1,57 @@
 "use client";
 
 import { useEffect, useState } from "react";
+//import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import userImage from "@/images/user.png";
 
 function TimeSlots() {
-  const [dayName, setDayName] = useState("sunday");
+  const today = new Date();
+  let datesList = [];
+  for(let i = 0; i < 7; i++) {
+    let newDate = new Date();
+    newDate.setDate(today.getDate() + i + 1);
+    datesList.push(newDate);
+  }
+  
+  const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
-  const [timesChosen, setTimesChosen] = useState({
-    sunday: new Set<String>(),
-    monday: new Set<String>(),
-    tuesday: new Set<String>(),
-    wednesday: new Set<String>(),
-    thursday: new Set<String>(),
-    friday: new Set<String>(),
-    saturday: new Set<String>(),
-  });
+  const timesList = {
+    "9:00 AM : 10:00 AM" : "9:00:00",
+    "10:00 AM : 11:00 AM" : "10:00:00",
+    "11:00 AM : 12:00 PM" : "11:00:00",
+    "12:00 PM : 1:00 PM" : "12:00:00",
+    "1:00 PM : 2:00 PM": "13:00:00",
+    "2:00 PM : 3:00 PM" : "14:00:00",
+    "3:00 PM : 4:00 PM" : "15:00:00",
+    "4:00 PM : 5:00 PM" : "16:00:00",
+    "5:00 PM : 6:00 PM" : "17:00:00",
+    "6:00 PM : 7:00 PM" : "18:00:00",
+    "7:00 PM : 8:00 PM" : "19:00:00",
+    "8:00 PM : 9:00 PM" : "20:00:00"
+  }
 
-  const [oldTimes, setOldTimes] = useState({
-    sunday: new Set<String>(),
-    monday: new Set<String>(),
-    tuesday: new Set<String>(),
-    wednesday: new Set<String>(),
-    thursday: new Set<String>(),
-    friday: new Set<String>(),
-    saturday: new Set<String>(),
-  });
+  const [dayDate, setDayDate] = useState(datesList[0].toDateString());
+
+  const [timesChosen, setTimesChosen] = useState(
+    Object.fromEntries(datesList.map(date => [
+    date.toDateString(), new Set<String>()
+  ])));
+
+  const [oldTimes, setOldTimes] = useState(
+    Object.fromEntries(datesList.map(date => [
+    date.toDateString(), new Set<String>()
+  ])));
 
   const [toggleChecked, setToggleChecked] = useState(false);
 
-  const daysList = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-  const timesList = [ "9:00 AM : 10:00 AM", "10:00 AM : 11:00 AM", "11:00 AM : 12:00 PM", "12:00 PM : 1:00 PM",
-                      "1:00 PM : 2:00 PM", "2:00 PM : 3:00 PM", "3:00 PM : 4:00 PM", "4:00 PM : 5:00 PM", "5:00 PM : 6:00 PM",
-                      "6:00 PM : 7:00 PM", "7:00 PM : 8:00 PM", "8:00 PM : 9:00 PM"];
+  /*useEffect(() => {
+    fetch("", { mode: "cors" })
+      .then((response) => response.json())
+      .then((response) => setOldTimes(() => response))
+      .catch((error) => console.error(error));
+  }, []);*/
 
   const baseButtonClass = "text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2";
   const dayButtonClass = "bg-blue-500 hover:bg-blue-800 " + baseButtonClass;
@@ -48,14 +66,14 @@ function TimeSlots() {
                         "after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-red-600"].join(" ");
 
   function getTimeClass(time: string) {
-    if(oldTimes[dayName as keyof typeof oldTimes].has(time)) {
+    if(oldTimes[dayDate].has(time)) {
       if(toggleChecked) {
         return timeButtonClass; // in the case of deletion we want the already selected times to be available for deletion
       }
       return disabledTimeClass;
     }
 
-    if(timesChosen[dayName as keyof typeof timesChosen].has(time)) {
+    if(timesChosen[dayDate].has(time)) {
       return clickedTimeClass;
     }
 
@@ -70,16 +88,16 @@ function TimeSlots() {
     e.preventDefault();
     const name = e.currentTarget.name;
 
-    if(dayName === name) {
+    if(dayDate === name) {
       return;
     }
 
-    setDayName(() => (name));
+    setDayDate(() => (name));
   };
 
   const handleTimeClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    let currSet = timesChosen[dayName as keyof typeof timesChosen];
+    let currSet = timesChosen[dayDate];
 
     if(e.currentTarget.className === clickedTimeClass) {
       e.currentTarget.className = timeButtonClass;
@@ -89,26 +107,27 @@ function TimeSlots() {
       e.currentTarget.className = clickedTimeClass;
       currSet.add(e.currentTarget.name);
     }
-    setTimesChosen((prevTimes) => ({...prevTimes, dayName: currSet,}));
+    setTimesChosen((prevTimes: any) => ({...prevTimes, dayDate: currSet,}));
   };
 
   const handleChangeToggle = () => {
-    setTimesChosen(() => ({
-      sunday: new Set<String>(),
-      monday: new Set<String>(),
-      tuesday: new Set<String>(),
-      wednesday: new Set<String>(),
-      thursday: new Set<String>(),
-      friday: new Set<String>(),
-      saturday: new Set<String>(),
-    }));
+    setTimesChosen(() => (
+      Object.fromEntries(datesList.map(date => [
+        date.toDateString(), new Set<String>()
+    ]))));
 
     setToggleChecked(() => (!toggleChecked));
   };
 
+  /*const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    axios.post("", timesChosen).then((response) => {
+      console.log(response.status, response.data.token);
+    });
+  };*/
+
   const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log(timesChosen);
   };
 
   return (
@@ -119,8 +138,8 @@ function TimeSlots() {
       </div>
       <div className="flex-initial m-5 bg-white rounded-xl relative max-w-lg min-w-0 md:basis-7/12 md:max-w-full">
         <div className="flex pt-4 mb-3">
-        <Link href="/doctorProfile/view" className="font-bold ml-7 w-1/2">Personal Information</Link>
-        <Link href="/" className="text-blue-500 font-bold ml-7 mr-7 md:mr-0 w-1/2">Time Slots</Link>
+          <Link href="/doctorProfile/view" className="font-bold ml-7 w-1/2">Personal Information</Link>
+          <Link href="/doctorProfile/timeSlots" className="text-blue-500 font-bold ml-7 mr-7 md:mr-0 w-1/2">Time Slots</Link>
         </div>
         <div className="flex">
             <hr className="bg-neutral-800 border-none h-0.5 w-1/2"></hr>
@@ -129,13 +148,21 @@ function TimeSlots() {
         <div className="p-7 relative">
           <div className="flex gap-28 mt-3">
             <div className="flex flex-col gap-5">
-              {daysList.map((day) => {
-                return <button key={day} name={day} onClick={handleDayClick} className={day === dayName ? clickedDayClass : dayButtonClass}>{day}</button>;
+              {datesList.map((date) => {
+                return <button
+                        key={days[date.getDay()]}
+                        name={date.toDateString()}
+                        onClick={handleDayClick}
+                        className={date.toDateString() === dayDate ? clickedDayClass : dayButtonClass}>
+                          {`${days[date.getDay()]} (${date.getDate()} / ${today.getMonth() + 1} / ${today.getFullYear()})`}
+                        </button>;
               })}
             </div>
             <div className="grid grid-cols-3 gap-5">
-              {Object.keys(timesList).map((time) => {
-                return <button key={time} name={time} onClick={handleTimeClick} className={getTimeClass(time)}>{time}</button>;
+              {Object.entries(timesList).map((timeEntry) => {
+                return <button key={timeEntry[0]} name={timeEntry[1]} onClick={handleTimeClick} className={getTimeClass(timeEntry[1])}>
+                         {timeEntry[0]}
+                       </button>;
               })}
             </div>
           </div>
