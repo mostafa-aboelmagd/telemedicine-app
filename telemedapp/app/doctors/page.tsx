f0c0accff2300780ce0ca1643a92e66d3511dee6
AssemSadek90 <101525092+AssemSadek90@users.noreply.github.com>
2024-09-09 03:51:25 +0300

    fix bugs in filter.

--- telemedapp/app/doctors/page.tsx
+++ telemedapp/app/doctors/page.tsx
@@ -10,21 +10,46 @@ import { IoFilter } from "react-icons/io5";
 
 const Doctors = () => {
     const [minMaxFees, setMinMaxFees] = useState({ min: 0, max: 1000 });
-    const doctors: any = [
-        { id: "1", name: "Dr. Ahmed Ali", nearestApp: new Date("2024-08-31"), title: "Psychiatrist", rating: 4.2, numSessions: 825, numReviews: 860, fees60min: 200, fees30min: 100, image: "/assets/doctorM.jpg", interests: ["Separation Anxiety Disorder, Generalized Anxiety Disorder, and Social Phobia", "Relations"], country: "Egypt", language: ["Arabic", "Mandarin"], gender: "Male", isOnline: "true" },
-        { id: "2", name: "Dr. Mona Aldahan", nearestApp: new Date("2024-09-01"), title: "Gynecologist", rating: 3.9, numSessions: 900, numReviews: 950, fees60min: 250, fees30min: 125, image: "/assets/doctorF.jpg", interests: ["Prenatal Care", "Reproductive Health", "Menopause"], country: "Saudi Arabia", language: ["Arabic", "Mandarin"], gender: "Female", isOnline: "false" },
-        { id: "3", name: "Dr. Amina Sameeh", nearestApp: new Date("2024-09-02"), title: "Ophthalmologist", rating: 3.4, numSessions: 780, numReviews: 800, fees60min: 220, fees30min: 110, image: "/assets/doctorF.jpg", interests: ["Cataract Surgery", "Glaucoma", "Vision Correction"], country: "Kuwait", language: ["English", "Mandarin"], gender: "Female", isOnline: "false" },
-        { id: "4", name: "Dr. Mohammed Mamdouh", nearestApp: new Date("2024-09-03"), title: "Plastic Surgeon", rating: 4.8, numSessions: 650, numReviews: 700, fees60min: 300, fees30min: 150, image: "/assets/doctorM.jpg", interests: ["Cosmetic Surgery", "Reconstructive Surgery", "Burn Treatment"], country: "Qatar", language: ["English", "Mandarin"], gender: "Male", isOnline: "true" },
-        { id: "5", name: "Dr. Yousef Karim", nearestApp: new Date("2024-09-04"), title: "Cardiologist", rating: 2.9, numSessions: 1000, numReviews: 1050, fees60min: 280, fees30min: 140, image: "/assets/doctorM.jpg", interests: ["Heart Disease", "Hypertension", "Cardiac Rehabilitation"], country: "Yemen", language: ["Arabic", "Mandarin"], gender: "Male", isOnline: "false" },
-        { id: "6", name: "Dr. Omar Tareq", nearestApp: new Date("2024-09-05"), title: "Dermatologist", rating: 2.4, numSessions: 850, numReviews: 900, fees60min: 230, fees30min: 115, image: "/assets/doctorM.jpg", interests: ["Skin Cancer", "Acne", "Eczema"], country: "Germany", language: ["German", "Mandarin"], gender: "Male", isOnline: "true" },
-        { id: "7", name: "Dr. Ayah Lotfy", nearestApp: new Date("2024-09-06"), title: "Neurologist", rating: 1.9, numSessions: 720, numReviews: 750, fees60min: 260, fees30min: 130, image: "/assets/doctorF.jpg", interests: ["Epilepsy", "Stroke", "Multiple Sclerosis"], country: "Morocco", language: ["English", "Mandarin"], gender: "Female", isOnline: "false" },
-        { id: "8", name: "Dr. Mostafa Barakat", nearestApp: new Date("2024-09-07"), title: "Orthopedic Surgeon", rating: 4.6, numSessions: 680, numReviews: 700, fees60min: 270, fees30min: 135, image: "/assets/doctorM.jpg", interests: ["Joint Replacement", "Sports Injuries", "Fracture Treatment"], country: "Morocco", language: ["Arabic", "Mandarin"], gender: "Male", isOnline: "true" },
-        { id: "9", name: "Dr. Mohsen Hassan", nearestApp: new Date("2024-09-08"), title: "Pediatrician", rating: 1.4, numSessions: 950, numReviews: 1000, fees60min: 210, fees30min: 105, image: "/assets/doctorM.jpg", interests: ["Child Development", "Vaccinations", "Pediatric Nutrition"], country: "Egypt", language: ["English", "Mandarin"], gender: "Male", isOnline: "true" },
-        { id: "10", name: "Dr. Sama Yehya", nearestApp: new Date("2024-09-09"), title: "Endocrinologist", rating: 0.9, numSessions: 800, numReviews: 850, fees60min: 240, fees30min: 120, image: "/assets/doctorF.jpg", interests: ["Diabetes", "Thyroid Disorders", "Hormonal Imbalances"], country: "Kuwait", language: ["Arabic", "Mandarin"], gender: "Female", isOnline: "false" }
-    ];
+    // const doctors: any = [
+    //     { id: "1", name: "Dr. Ahmed Ali", nearestApp: new Date("2024-08-31"), title: "Psychiatrist", rating: 4.2, numSessions: 825, numReviews: 860, fees60min: 200, fees30min: 100, image: "/assets/doctorM.jpg", interests: ["Separation Anxiety Disorder, Generalized Anxiety Disorder, and Social Phobia", "Relations"], country: "Egypt", language: ["Arabic", "Mandarin"], gender: "Male", isOnline: "true" },
+    //     { id: "2", name: "Dr. Mona Aldahan", nearestApp: new Date("2024-09-01"), title: "Gynecologist", rating: 3.9, numSessions: 900, numReviews: 950, fees60min: 250, fees30min: 125, image: "/assets/doctorF.jpg", interests: ["Prenatal Care", "Reproductive Health", "Menopause"], country: "Saudi Arabia", language: ["Arabic", "Mandarin"], gender: "Female", isOnline: "false" },
+    //     { id: "3", name: "Dr. Amina Sameeh", nearestApp: new Date("2024-09-02"), title: "Ophthalmologist", rating: 3.4, numSessions: 780, numReviews: 800, fees60min: 220, fees30min: 110, image: "/assets/doctorF.jpg", interests: ["Cataract Surgery", "Glaucoma", "Vision Correction"], country: "Kuwait", language: ["English", "Mandarin"], gender: "Female", isOnline: "false" },
+    //     { id: "4", name: "Dr. Mohammed Mamdouh", nearestApp: new Date("2024-09-03"), title: "Plastic Surgeon", rating: 4.8, numSessions: 650, numReviews: 700, fees60min: 300, fees30min: 150, image: "/assets/doctorM.jpg", interests: ["Cosmetic Surgery", "Reconstructive Surgery", "Burn Treatment"], country: "Qatar", language: ["English", "Mandarin"], gender: "Male", isOnline: "true" },
+    //     { id: "5", name: "Dr. Yousef Karim", nearestApp: new Date("2024-09-04"), title: "Cardiologist", rating: 2.9, numSessions: 1000, numReviews: 1050, fees60min: 280, fees30min: 140, image: "/assets/doctorM.jpg", interests: ["Heart Disease", "Hypertension", "Cardiac Rehabilitation"], country: "Yemen", language: ["Arabic", "Mandarin"], gender: "Male", isOnline: "false" },
+    //     { id: "6", name: "Dr. Omar Tareq", nearestApp: new Date("2024-09-05"), title: "Dermatologist", rating: 2.4, numSessions: 850, numReviews: 900, fees60min: 230, fees30min: 115, image: "/assets/doctorM.jpg", interests: ["Skin Cancer", "Acne", "Eczema"], country: "Germany", language: ["German", "Mandarin"], gender: "Male", isOnline: "true" },
+    //     { id: "7", name: "Dr. Ayah Lotfy", nearestApp: new Date("2024-09-06"), title: "Neurologist", rating: 1.9, numSessions: 720, numReviews: 750, fees60min: 260, fees30min: 130, image: "/assets/doctorF.jpg", interests: ["Epilepsy", "Stroke", "Multiple Sclerosis"], country: "Morocco", language: ["English", "Mandarin"], gender: "Female", isOnline: "false" },
+    //     { id: "8", name: "Dr. Mostafa Barakat", nearestApp: new Date("2024-09-07"), title: "Orthopedic Surgeon", rating: 4.6, numSessions: 680, numReviews: 700, fees60min: 270, fees30min: 135, image: "/assets/doctorM.jpg", interests: ["Joint Replacement", "Sports Injuries", "Fracture Treatment"], country: "Morocco", language: ["Arabic", "Mandarin"], gender: "Male", isOnline: "true" },
+    //     { id: "9", name: "Dr. Mohsen Hassan", nearestApp: new Date("2024-09-08"), title: "Pediatrician", rating: 1.4, numSessions: 950, numReviews: 1000, fees60min: 210, fees30min: 105, image: "/assets/doctorM.jpg", interests: ["Child Development", "Vaccinations", "Pediatric Nutrition"], country: "Egypt", language: ["English", "Mandarin"], gender: "Male", isOnline: "true" },
+    //     { id: "10", name: "Dr. Sama Yehya", nearestApp: new Date("2024-09-09"), title: "Endocrinologist", rating: 0.9, numSessions: 800, numReviews: 850, fees60min: 240, fees30min: 120, image: "/assets/doctorF.jpg", interests: ["Diabetes", "Thyroid Disorders", "Hormonal Imbalances"], country: "Kuwait", language: ["Arabic", "Mandarin"], gender: "Female", isOnline: "false" }
+    // ];
 
-    // const [doctors, setDoctors] = useState<any>([]);
-    const [loading, setLoading] = useState<boolean>(true)
+    const [doctors, setDoctors] = useState<any>([]);
+
+    const [specializationOptions, setSpecializationOptions] = useState<any>([]);
+    const [countryOptions, setCountryOptions] = useState<any>([]);
+    const [languageOptions, setLanguageOptions] = useState<any>([]);
+
+    const handleFillOptions = (doctors: any[]) => {
+        let speciality: any = [];
+        let country: any = [];
+        let language: any = [];
+        doctors.forEach((doctor: any) => {
+            if (!speciality.includes(doctor.title)) {
+                speciality.push({ value: doctor.title, label: doctor.title });
+            }
+            if (!country.includes(doctor.country)) {
+                country.push({ value: doctor.country, label: doctor.country });
+            }
+            doctor.language.forEach((lang: string) => {
+                if (!language.includes(lang)) {
+                    language.push({ value: lang, label: lang });
+                }
+            });
+        });
+        setSpecializationOptions(speciality);
+        setCountryOptions(country);
+        setLanguageOptions(language);
+    }
 
     const headers = {
         "Content-Type": "application/json"
@@ -35,9 +60,10 @@ const Doctors = () => {
             console.log("Error: Request sent no data")
         }
         else {
-            setLoading(false)
             const data = await response.json();
-            // setDoctors(data)
+            handleFillOptions(data)
+            setDoctors(data)
+            // handleShownDoctors(filters);
         }
     }
     const [filters, setFilters] = useState({ speciality: "", gender: "", rating: "", price: [], todayDate: "", thisWeek: "", dateRange1: "", dateRange2: "", country: [], language: [], sort: "", isOnline: "" });
@@ -158,10 +184,14 @@ const Doctors = () => {
         { value: 'reset', label: 'Reset' }
     ];
     useEffect(() => {
-        // fetchDoctors()
+        fetchDoctors()
         getMinMaxFees()
-        handleShownDoctors(filters);
-    }, [filters]);
+    }, []);
+    useEffect(() => {
+        if (doctors.length > 0) {
+            handleShownDoctors(filters);
+        }
+    }, [doctors, filters]);
     return (
         <main className='space-y-12'>
             <ReadyTherapist />
@@ -172,8 +202,8 @@ const Doctors = () => {
                 <div className='md:hidden ml-4 text-[#035fe9]'>
                     <button onClick={handleOpenModal} className='flex items-center'>Filters <IoFilter className='w-4 h-4 ml-2' /></button>
                 </div>
-                <FilterComponent minFees={minMaxFees.min} maxFees={minMaxFees.max} handleOpenModal={handleOpenModal} openModal={openModal} handleChangeOptions={handleChangeOptions} handleChangeFilterDrop={handleChangeFilterDrop} handleResetFilters={handleResetFilters} />
-                {doctors ? <DoctorGrid doctors={filteredDoctors} /> : <div> Loading...</div>}
+                <FilterComponent specializationOptions={specializationOptions} countryOptions={countryOptions} languageOptions={languageOptions} minFees={minMaxFees.min} maxFees={minMaxFees.max} handleOpenModal={handleOpenModal} openModal={openModal} handleChangeOptions={handleChangeOptions} handleChangeFilterDrop={handleChangeFilterDrop} handleResetFilters={handleResetFilters} />
+                {filteredDoctors.length > 0 ? <DoctorGrid doctors={filteredDoctors} /> : <div> Loading...</div>}
             </section>
         </main>
     )
