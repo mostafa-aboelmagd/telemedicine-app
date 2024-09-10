360e13d8ed4f3df1409ebc6d4c325545e1776829
Youssef Hassanien <Youssefhassanein17@gmail.com>
2024-09-10 10:25:49 +0300

    app.js modifications

--- app.js
+++ app.js
@@ -2,21 +2,20 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('dotenv').config();
-const patientLoginRoute = require('./Routes/Patient/Login');
+const userLoginRoute = require('./Routes/Login');
 const patientRegisterRoute = require('./Routes/Patient/Register');
 const patienProfileRoute = require('./Routes/Patient/Profile');
 const patientEditRoute = require('./Routes/Patient/Edit');
 const patientAppointmentBookRoute = require('./Routes/Patient/Appointment/Book');
 const patientHomeRoute = require('./Routes/Patient/Home');
-const doctorLoginRoute = require('./Routes/Login');
+const patientAddPrescriptionRoute = require('./Routes/Patient/MedicalHistory/addPrescription');
+const patientDeletePrescriptionRoute= require('./Routes/Patient/MedicalHistory/deletePrescription');
+const patientViewPrescriptionRoute= require('./Routes/Patient/MedicalHistory/viewPrescription');
 const doctorProfileRoute = require('./Routes/Doctor/Profile');
 const doctorEditRoute = require('./Routes/Doctor/Edit');
 const doctorAddAvailabilityRoute = require('./Routes/Doctor/Availability/add');
 const doctorDeleteAvailabilityRoute = require('./Routes/Doctor/Availability/Delete');
 const doctorEditAvailabilityRoute = require('./Routes/Doctor/Availability/edit');
-const patientAddPrescriptionRoute = require('./Routes/Patient/MedicalHistory/addPrescription');
-const patientDeletePrescriptionRoute= require('./Routes/Patient/MedicalHistory/deletePrescription');
-const patientViewPrescriptionRoute= require('./Routes/Patient/MedicalHistory/viewPrescription');
 const port = process.env.PORT || 4000;
 const app = express();
 
@@ -32,21 +31,21 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.use('/patient/login', patientLoginRoute);
+app.use('/login', userLoginRoute);
 app.use('/patient/register', patientRegisterRoute);
 app.use('/patient/profile', patienProfileRoute);
 app.use('/patient/edit', patientEditRoute);
 app.use('/patient/appointment/book', patientAppointmentBookRoute);
 app.use('/patient/home', patientHomeRoute);
-app.use('/doctor/login', doctorLoginRoute);
+app.use('/patient/prescription/add',patientAddPrescriptionRoute);
+app.use('/patient/prescription/view',patientViewPrescriptionRoute);
+app.use('/patient/prescription/delete',patientDeletePrescriptionRoute);
 app.use('/doctor/edit', doctorEditRoute);
 app.use('/doctor/profile', doctorProfileRoute);
 app.use('/doctor/availability/add', doctorAddAvailabilityRoute);
 app.use('/doctor/availability/delete', doctorDeleteAvailabilityRoute);
 app.use('/doctor/availability/edit', doctorEditAvailabilityRoute);
-app.use('/patient/prescription/add',patientAddPrescriptionRoute);
-app.use('/patient/prescription/view',patientViewPrescriptionRoute);
-app.use('/patient/prescription/delete',patientDeletePrescriptionRoute)
+
 
 
 app.use((error, req, res, next) => {
