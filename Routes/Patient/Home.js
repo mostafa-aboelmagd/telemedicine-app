fef1e63aa0c8903f8d559dcb5d8ba164b88573db
Youssef Hassanien <Youssefhassanein17@gmail.com>
2024-09-11 07:56:32 +0300

    Removed authentication from patients home page

--- Routes/Patient/Home.js
+++ Routes/Patient/Home.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const patientHomeController = require('../../Controllers/Patient/Home');
-const { tokenAuthentication } = require('../../Middleware/User/Authentication');
 const router = express.Router();
 
-router.get('', tokenAuthentication, patientHomeController.home);
+router.get('', patientHomeController.home);
 
 module.exports = router;
