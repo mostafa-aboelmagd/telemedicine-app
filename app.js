9ce24eb0688394fe31f0479cd1bdd07b3e1deef8
Youssef Hassanien <Youssefhassanein17@gmail.com>
2024-09-14 03:18:56 +0300

    Cors modifications

--- app.js
+++ app.js
@@ -43,9 +43,9 @@ const swaggerSpec = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(cors({
-  origin: 'https://tele-med-pilot.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'multipart/form-data', "Accept", "Origin", "X-Requested-With"],
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'multipart/form-data', "Accept", "Origin", "X-Requested-With","Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"],
   credentials: true,
   preflightContinue: false,
 }));
