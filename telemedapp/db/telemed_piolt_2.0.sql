99f9c367894ba90520e4272350ae99adf28b21d6
Ahmed Al-Deeb <124098788+AhmedXAlDeeb@users.noreply.github.com>
2024-09-18 19:33:21 +0300

    creation code for the modified db

--- telemedapp/db/telemed_piolt_2.0.sql
+++ telemedapp/db/telemed_piolt_2.0.sql
@@ -21,7 +21,9 @@ create table users
                    ((ARRAY ['Male'::character varying, 'Female'::character varying])::text[])),
     user_first_name    varchar(255),
     user_last_name     varchar(255),
-    user_birth_year    integer
+    user_birth_year    integer,
+    created_at                   timestamp default CURRENT_TIMESTAMP,
+    updated_at                   timestamp default CURRENT_TIMESTAMP
 );
 
 create table doctor
@@ -40,7 +42,7 @@ create table doctor
     updated_at                    timestamp default CURRENT_TIMESTAMP,
     doctor_image                  bytea,
     doctor_table_id               smallint  default nextval('doctors_doctor_user_id_seq'::regclass) not null,
-    doctor_nearest_appointment_id integer,
+    doctor_nearest_appointment_reference integer, -- will reference the nearest appointment for the doctor
     doctor_city                   varchar,
     doctor_clinic_location        varchar
 );
@@ -61,7 +63,9 @@ create table patient
         constraint patients_current_doctor_id_fkey
             references doctor,
     patient_wallet            integer,
-    patient_table_id          smallint default nextval('patients_patient_user_id_seq'::regclass) not null
+    patient_table_id          smallint default nextval('patients_patient_user_id_seq'::regclass) not null,
+    created_at                   timestamp default CURRENT_TIMESTAMP,
+    updated_at                   timestamp default CURRENT_TIMESTAMP
 );
 
 alter sequence patients_patient_id_seq owned by patient.patient_user_id_reference;
@@ -82,11 +86,15 @@ create table review
     review_stars      smallint
         constraint reviews_stars_check
             check ((review_stars > 0) AND (review_stars < 6)),
-    review_date       timestamp
+    review_date       timestamp,
+    created_at                   timestamp default CURRENT_TIMESTAMP,
+    updated_at                   timestamp default CURRENT_TIMESTAMP
 );
 
 create table doctor_experience
 (
+    doctor_experience_id        serial
+        primary key,
     doctor_experience_doctor_id  integer
         constraint doctor_experience_doctor_id_fkey
             references doctor
@@ -95,11 +103,15 @@ create table doctor_experience
     doctor_experience_firm_name  varchar,
     doctor_experience_department varchar,
     doctor_experience_start_date date,
-    doctor_experience_end_date   date
+    doctor_experience_end_date   date,
+    created_at                   timestamp default CURRENT_TIMESTAMP,
+    updated_at                   timestamp default CURRENT_TIMESTAMP
 );
 
 create table doctor_education
 (
+    doctor_education_id        serial
+        primary key,
     education_doctor_id   integer
         constraint education_doctor_id_fkey
             references doctor
@@ -107,11 +119,15 @@ create table doctor_education
     education_certificate varchar,
     education_authority   varchar,
     education_start_date  date,
-    education_end_data    date
+    education_end_data    date,
+    created_at                   timestamp default CURRENT_TIMESTAMP,
+    updated_at                   timestamp default CURRENT_TIMESTAMP
 );
 
 create table doctor_interest
 (
+    doctor_interest_id        serial
+        primary key,
     doctor_interest_doctor_id integer
         constraint doctor_interest_doctor_id_fkey
             references doctor
@@ -119,7 +135,9 @@ create table doctor_interest
     doctor_interest_name      varchar,
     doctor_interest_category  varchar,
     interest_id               serial
-        primary key
+        primary key,
+    created_at                   timestamp default CURRENT_TIMESTAMP,
+    updated_at                   timestamp default CURRENT_TIMESTAMP
 );
 
 create table doctor_availability
@@ -128,45 +146,22 @@ create table doctor_availability
         constraint doctor_availability_doctor_id_fkey
             references doctor
             on delete cascade,
+    doctor_availability_type    varchar(16) constraint users_role_check
+            check ((doctor_availability_status)::text = ANY
+                   (ARRAY [('Online'::character varying)::text, ('Onsite'::character varying)::text])),
     doctor_availability_status    varchar(16) constraint users_role_check
             check ((doctor_availability_status)::text = ANY
                    (ARRAY [('Booked'::character varying)::text, ('Pending'::character varying)::text, ('Available'::character varying)::text])),
+
     doctor_availability_day_hour  timestamp,
     doctor_availability_id        serial
-        primary key
+        primary key,
+    created_at                   timestamp default CURRENT_TIMESTAMP,
+    updated_at                   timestamp default CURRENT_TIMESTAMP
 );
 
 
-create table appointment
-(
-    appointment_patient_id        integer
-        constraint appointment_patient_id_fkey
-            references patient
-            on delete cascade,
-    appointment_doctor_id         integer
-        constraint appointment_doctor_id_fkey
-            references doctor
-            on delete cascade,
-    appointment_type              varchar,
-    appointment_duration          integer,
-    appointment_id                serial
-        constraint pk_appointment
-            primary key,
-    appointment_status            varchar
-        constraint check_appointment_status
-            check ((appointment_status)::text = ANY
-                   (ARRAY ['scheduled'::text, 'finished'::text, 'running'::text, 'unbooked'::text])),
-    appointment_availability_slot integer default nextval('appointment_slot_seq'::regclass) not null
-        constraint appointment_slot_fkey
-            references doctor_availability
-            on delete cascade,
-    appointment_location          varchar
-        constraint appointment_appointment_location_check
-            check ((appointment_location)::text = ANY
-                   ((ARRAY ['online'::character varying, 'onsite'::character varying])::text[]))
-);
 
-alter sequence appointment_slot_seq owned by appointment.appointment_availability_slot;
 
 create table languages
 (
@@ -199,6 +194,7 @@ create table appointment_review
             check ((appointment_review_commitment_rating > 0) AND (appointment_review_commitment_rating < 6))
 );
 
+-- we will delete the prescription table and merge it with the follow up table
 create table prescriptions
 (
     prescription_id              serial
@@ -217,43 +213,66 @@ create table prescriptions
     updated_at                   timestamp default CURRENT_TIMESTAMP
 );
 
-create table appointment_request
+
+create table request_appointment
 (
-    appointment_request_patient_id        integer
+    request_appointment_patient_id        integer
         constraint appointment_request_patient_id_fkey
             references patient
             on delete cascade,
-    appointment_request_doctor_id         integer
+    request_appointment_doctor_id         integer
         constraint appointment_request_doctor_id_fkey
             references doctor
             on delete cascade,
-    appointment_request_aviliability_id     integer not null
-        constraint appointment_request_aviliability_id_fkey
+    request_appointment_availability_slot integer default nextval('appointment_slot_seq'::regclass) not null
+        constraint appointment_slot_fkey
             references doctor_availability
             on delete cascade,
-    appointment_request_complaint_id text not null,
-    appointment_request_status varchar check ((appointment_request_status)::text = ANY
-                   (ARRAY ['Approved'::text, 'Chat'::text, 'Waiting'::text, 'Declined'::text]))
-);
+    request_appointment_type              varchar
+        constraint check_appointment_type
+            check ((request_appointment_type)::text = ANY
+                   (ARRAY ['Followup'::text, 'Investigation'::text])),
+    request_appointment_duration          integer,
+    request_appointment_id                serial
+        constraint pk_request_appointment
+            primary key,
+    request_appointment_complaint text not null,
 
-create table prescription_medications
+    request_appointment_location          varchar
+        constraint appointment_appointment_location_check
+            check ((request_appointment_location)::text = ANY
+                   ((ARRAY ['online'::character varying, 'onsite'::character varying])::text[])),
+
+    request_appointment_status varchar check ((request_appointment_status)::text = ANY
+                   (ARRAY ['Approved'::text, 'Chat'::text, 'Waiting'::text, 'Declined'::text])),
+    created_at                   timestamp default CURRENT_TIMESTAMP,
+    updated_at                   timestamp default CURRENT_TIMESTAMP
+);
+alter sequence appointment_slot_seq owned by request_appointment.request_appointment_availability_slot;
+
+
+create table medications_plan
 (
-    prescription_medication_id           serial
+    medication_plan_id           serial
         primary key,
-    prescription_medication_reference_id integer
-        constraint prescription_medications_prescription_medication_reference_fkey
-            references prescriptions,
-    prescription_medication_name         varchar(255),
-    prescription_medications_dosage      varchar(255),
-    prescription_medications_note        text,
-    prescription_medications_start_date  date,
-    prescription_medications_end_date    date
+    medication_plan_appointment_reference_id integer
+        constraint medication_plan_appointment_reference_id_fkey
+            references request_appointment,
+    medication_plan_name         varchar(255),
+    medication_plan_dosage      varchar(255),
+    medication_plan_note        text,
+    medication_plan_start_date  date,
+    medication_plan_end_date    date
 );
 
 create table medical_documents
 (
     medical_document_id          serial
         primary key,
+    medical_document_appointment_reference integer
+        constraint medical_document_appointment_reference_fkey
+            references appointment
+            on delete cascade,
     medical_documents_patient_id integer
         references patient,
     medical_document_type        varchar(255),
@@ -264,9 +283,22 @@ create table medical_documents
 
 );
 
-create table appointment_followup
+create table followup
 (
     --treatment plan
     --next appointment
-    --
+    --prescription
+    followup_diagnosis text not null ,
+
+    followup_appointment_reference integer
+        constraint followup_appointment_reference_fkey
+            references request_appointment
+            on delete cascade    ,
+    followup_next_appointment integer -- here when the value is not null it creates a new appointment with an accepted request
+        -- if the attribute of followup in the appointment table is true
+        constraint followup_next_appointment_reference_fkey
+        references appointment
+        on delete cascade ,
+    created_at                   timestamp default CURRENT_TIMESTAMP,
+    updated_at                   timestamp default CURRENT_TIMESTAMP
 );
