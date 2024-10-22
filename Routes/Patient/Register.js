d3daa0c0965f1041eaf654534c88d42d5f8cbd92
yahya-khalaf <yahya.khalaf54@gmail.com>
2024-10-22 13:11:46 +0300

    ya allahh

--- Routes/Patient/Register.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const registerController = require('../../Controllers/Patient/Register');
-
-const router = express.Router();
-
-router.post('', registerController.patientRegister);
-
-module.exports = router;
