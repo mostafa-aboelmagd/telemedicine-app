52412cde97caea70aa7d74139c794382ab9c93e6
yahya <yahya.khalaf54@gmail.com>
2024-12-23 13:57:07 +0200

    latest mobile app

--- Mobile App/teleMed_doctor/App.js
+++ Mobile App/teleMed_doctor/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import StackRouters from "./app/stackRouters";
-import { initializeApp } from 'firebase/app'; // Import Firebase initialization function
-const firebaseConfig = {
-  apiKey: "AIzaSyDfE0VimHcuMs8xetu1jIywme-_aytt4pE", 
-  authDomain: "telemedpilot.firebaseapp.com", 
-  projectId: "telemedpilot", 
-  storageBucket: "telemedpilot.appspot.com", 
-  messagingSenderId: "778585481356", 
-  appId: "1:778585481356:android:227de0b82fa2a82a149ba9" 
-};
-const app = initializeApp(firebaseConfig); 
+
 export default function App() {
   return (
-    <StackRouters />
+    <StackRouters/>
   );
 }
